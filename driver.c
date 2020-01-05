8704ad2e6ea208ddf9f73dc1fdb66e5c4ea633b5
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-05 22:47:54 +0000

    takes args from cmd line and runs through reader

--- driver.c
+++ driver.c
@@ -7,11 +7,13 @@
  */
 int main(int argc, char **argv)
 {
+	char *file = argv[1];
+
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	parser();
+	reader(file);
 	return (EXIT_SUCCESS);
 }
