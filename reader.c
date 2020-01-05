8385f6ef3e0ab979c147d52cf0c9751a2cd7849a
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-05 22:50:05 +0000

    opens file and reads

---
+++ reader.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * reader - reads a file line by line
+ * @file: file to be read
+ */
+void reader(char *file)
+{
+	FILE *fn;
+	stack_t *stack = NULL;
+	size_t size = 0;
+	unsigned int line = 0;
+	char **toks, *tok = NULL;
+
+	fn = fopen(file, "r");
+	while (-1 != getline(&tok, &size, fn))
+	{
+		line += 1;
+		toks = parser(tok);
+		if (toks == NULL)
+			continue;
+		else
+			get_op_func(toks, line)(&stack, line);
+	}
+	fclose(fn);
+	free(tok);
+	free_list(&stack);
+}
