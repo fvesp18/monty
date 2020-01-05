98de7eaeefb2e5c65d841bcca781ff91ac7940b3
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-05 21:26:33 +0000

    change file name

---
+++ op_get.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+ * get_op_func - calls operator function
+ * @s: type of operators
+ * Return: NULL, on success
+ */
+
+void (*get_op_func(char **toks, unsigned int line))(stack_t **, unsigned int)
+{
+	unsigned int count;
+	
+	instruction_t ops[] = {
+	{"push", op_push},
+	{"pall", op_pall},
+	{"pint", op_pint},
+	{"pop", op_pop},
+	{"swap", op_swap},
+	{"add", op_add},
+	{"nop", op_nop},
+	{NULL, NULL}
+	};
+
+	count = 0;
+	while (ops[count].opcode != NULL)
+	{
+		if ((strcmp(ops[count].opcode, toks[0]) == 0))
+		{
+			if ((strcmp(ops[count].opcode, "push") == 0) && toks[0][1] = NULL)
+			{
+				free(toks);
+				fprintf(stderr, "L%d: usage: push integer\n", line);
+				exit(EXIT_FAILURE);
+			}
+			else if ((strcmp(ops[count].opcode, "push") == 0))
+				num = atoi(toks[1]);
+			free(toks);
+			return(ops[count].f);
+		}
+		count++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line, toks[0]);
+	free(toks);
+	exit(EXIT_FAILURE);
+}
