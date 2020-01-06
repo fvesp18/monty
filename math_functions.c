cbedae28aa7530d6373281519a4829661d5ded44
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-06 02:52:44 +0000

    account for error messsage

--- math_functions.c
+++ math_functions.c
@@ -36,7 +36,7 @@ void op_sub(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
