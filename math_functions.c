bde68b312b84d0affaa8569fba50f702110fd6e9
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-06 02:51:17 +0000

    add sub func

--- math_functions.c
+++ math_functions.c
@@ -1,10 +1,8 @@
 #include "monty.h"
-
 /**
  * op_add - adds two elements of the stack
  * @stack: pointer to linked list in our stack structure
  * @line_number: line number
- * Return: sum of two elements
  */
 void op_add(stack_t **stack, unsigned int line_number)
 {
@@ -13,7 +11,7 @@ void op_add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -22,6 +20,31 @@ void op_add(stack_t **stack, unsigned int line_number)
 	element2 = new->next->n;
 	sum = element2 + element1;
 	new->next->n = sum;
+	*stack = new->next;
+	(*stack)->prev = NULL;
+	free(new);
+}
+/**
+ * op_sub - subtracts top two elements
+ * @stack: stack
+ * @line: line
+ */
+void op_sub(stack_t **stack, unsigned int line_number)
+{
+	int element1, element2, sub;
+	stack_t *new;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new = *stack;
+	element1 = new->n;
+	element2 = new->next->n;
+	sub = element2 - element1;
+	new->next->n = sub;
 	*stack = new->next;
 	(*stack)->prev = NULL;
 	free(new);
