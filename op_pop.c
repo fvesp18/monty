dc735f1e4d26c08c902b9fa522f986df9276bb68
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-06 01:53:55 +0000

    add new line

--- op_pop.c
+++ op_pop.c
@@ -10,7 +10,7 @@ void op_pop(stack_t **stack, unsigned int line)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line);
 		exit(EXIT_FAILURE);
 	}
 	*stack = buff->next;
