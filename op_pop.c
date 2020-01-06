e351f003654697fa9b8e746ad07ca9a26438dfe6
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-01-06 01:43:22 +0000

    removes top/ beginning element of stack

---
+++ op_pop.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * op_pop - removes head of stack
+ * @stack: stack
+ * @line: line
+ */
+void op_pop(stack_t **stack, unsigned int line)
+{
+	stack_t *buff = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack", line);
+		exit(EXIT_FAILURE);
+	}
+	*stack = buff->next;
+	if (buff->next)
+	{
+		buff->next->prev = NULL;
+	}
+	free(buff);
+}
